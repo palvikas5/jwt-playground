83315fb08968373bc294d1b51e33a8a5fafe0c8c
Vikas Pal <palvikas5@gmail.com>
2020-11-26 23:56:56 +0530

    Integrate with Sentry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import { Layout } from '../Layout/Layout';
+
+Sentry.init({
+  dsn:
+    'https://8d5650188e3d46dd9d9e76f30f934dc8@o482995.ingest.sentry.io/5534045',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 interface AppProps {
   Component: React.ComponentType;
@@ -15,6 +27,9 @@ const App = ({ Component, pageProps }: AppProps) => {
           key='fonts'
           href='https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap'
         />
+        <title key='title'>
+          JWT Playground | Decode, Sign and Verify JWT Tokens
+        </title>
       </Head>
       <Layout>
         <Component {...pageProps} />
